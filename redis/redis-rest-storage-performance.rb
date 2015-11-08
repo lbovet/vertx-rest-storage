89c2a25457ad515d71080a4b7599437bcbf337a7
Florian Kammermann <florian@kammermann.me>
2015-11-08 21:54:35 +0100

    added documentation

--- redis/redis-rest-storage-performance.rb
+++ redis/redis-rest-storage-performance.rb
@@ -1,7 +1,25 @@
 #!/usr/bin/ruby
 
+############################################################################################
+# Evalutes the time spent in redis for a rest-storage call
+# The execution time is collected over the redis slowlog command
+# http://redis.io/commands/slowlog
+# You have to consider, that the slowlog only has 128 entries max
+#
+# usage: ruby redis-rest-storage-performance.rb -u http://host:port/rest/resource -s 500
+# u is the url to call / evaluate
+# s is the number of calls / samples to execute
+############################################################################################
 require 'optparse'
 
+############################################################################################
+# Parse the redis slowlog and create an object of every entry
+# Because we have no separator, we take the unique progressive identifier as identifier 
+# for a new entry
+# 
+# slowlog: the slowlog of the actual run
+# redis_calls: the redis_calls the executed runs
+############################################################################################
 def parse_slowlog(slowlog, redis_calls)
 	first_id = slowlog.length-5
 	start_id = slowlog[first_id].to_i
@@ -51,44 +69,51 @@ def parse_slowlog(slowlog, redis_calls)
 	return redis_calls
 end
 
+############################################################################################
+# Sets the limit to enter the slowlog for a command to 1 microsecond.
+# Resets the slowlog
+# Executes the rest-storage call
+# Gets the slowlog
+############################################################################################
 def execute_rest_storage_and_read_slowlog(url)
 	`redis-cli config set slowlog-log-slower-than 1`
 	`redis-cli slowlog reset`
 	`curl -s #{url} > /dev/null`
-	#http://v00055:7012/nemo/server/tests/expand/10k/?expand=1
 	slowlog = `redis-cli slowlog get 128`
 	lines = slowlog.split(/\n+/)
-	#lines = File.readlines('slowlog')
 	return lines
 end
 
+############################################################################################
+# Parse the options
+############################################################################################
 options = {}
-
 optparse = OptionParser.new do |opts|
 	opts.banner = "test the performance of a rest-storage call"
-
 	opts.on("-u", "--url URL", "the url to call") do |url|
 		options[:url] =  url
 	end
-
 	opts.on("-s", "--samples SAMPLES", "the number of samples to take") do |samples|
 		options[:samples] =  samples
 	end
-
 end
-
 optparse.parse!
 
 samples = options[:samples].to_i
 url = options[:url]
 
+############################################################################################
+# Execute the calls
+############################################################################################
 redis_calls = Hash.new
-
 samples.times do |i|
 	lines = execute_rest_storage_and_read_slowlog(url)
 	redis_calls = parse_slowlog(lines, redis_calls)
 end
 
+############################################################################################
+# Collect the data of every call and calculate the average
+############################################################################################
 result = Hash.new
 redis_calls.each do |key,redis_call|
 	avg_time = redis_call[:time].inject{|sum,x| sum + x } / samples
@@ -98,6 +123,9 @@ result.sort_by {|key,time| time}.each do |key,value|
 	puts "time: " + value.to_s + " key: " + key
 end
 
+############################################################################################
+# Calculate the time spent in redis calls and the time spent in lua
+############################################################################################
 evalshaPlusRedis = result.clone.delete_if{|key,value| !key.match(/^evalsha.*/)}.values.inject{|sum,a| sum + a}
 onlyRedis = result.clone.delete_if{|key,value| key.match(/^evalsha.*/)}.values.inject{|sum,a| sum + a}
 puts "time in redis calls: " + onlyRedis.to_s
